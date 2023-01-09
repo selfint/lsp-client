c1973739c5cf0fb10c4b5700727fb6366dd6855e
Tom Selfin <selfint@gmail.com>
2023-01-09 19:31:16 +0200

    impl new method

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct LspClient<T> {
+    server: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> LspClient<T> {
+    pub fn new(server: T) -> Self {
+        Self { server }
     }
 }
