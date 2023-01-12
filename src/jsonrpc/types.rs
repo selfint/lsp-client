9ed05d767e0bb1f313fd3807625f7d566328f310
Tom Selfin <selfint@gmail.com>
2023-01-12 13:37:48 +0200

    implement json rpc types

--- src/jsonrpc/types.rs
+++ src/jsonrpc/types.rs
@@ -22,32 +22,55 @@ impl<P> Request<P> {
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 struct Response<R, E> {
     jsonrpc: String,
+    #[serde(flatten)]
     result: JsonRPCResult<R, E>,
     id: Option<usize>,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
-enum JsonRPCResult<R, E> {
-    Ok(R),
-    Err(JsonRPCError<E>),
+impl<R, E> Response<R, E> {
+    fn new(result: JsonRPCResult<R, E>, id: Option<usize>) -> Self {
+        Self {
+            jsonrpc: JSONRPC_V2.to_string(),
+            result,
+            id,
+        }
+    }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
+#[serde(rename_all = "lowercase")]
+enum JsonRPCResult<R, E> {
+    Result(R),
+    Error(JsonRPCError<E>),
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 struct JsonRPCError<D> {
     code: i32,
     message: String,
-    data: D,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    data: Option<D>,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 struct Notification<P> {
     jsonrpc: String,
     method: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     params: Option<P>,
+}
+
+impl<P> Notification<P> {
+    fn new(method: impl Into<String>, params: Option<P>) -> Self {
+        Self {
+            jsonrpc: JSONRPC_V2.to_string(),
+            method: method.into(),
+            params,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -61,14 +84,15 @@ mod tests {
     where
         D: Serialize + DeserializeOwned + PartialEq + Eq + std::fmt::Debug,
     {
-        let actual_deserialized = serde_json::from_value::<D>(expected_serialized.clone());
         let actual_serialized = serde_json::to_value(&expected_deserialized);
 
-        assert!(actual_deserialized.is_ok());
-        assert_eq!(actual_deserialized.unwrap(), expected_deserialized);
-
         assert!(actual_serialized.is_ok());
-        assert_eq!(actual_serialized.unwrap(), expected_serialized);
+        assert_eq!(expected_serialized, actual_serialized.unwrap());
+
+        let actual_deserialized = serde_json::from_value::<D>(expected_serialized);
+
+        assert!(actual_deserialized.is_ok());
+        assert_eq!(expected_deserialized, actual_deserialized.unwrap());
     }
 
     #[test]
@@ -92,9 +116,9 @@ mod tests {
             json!({
                 "jsonrpc": "2.0",
                 "method": "method",
-                "id": 1
+                "id": 2
             }),
-            Request::<()>::new("method", None, Some(1)),
+            Request::<()>::new("method", None, Some(2)),
         );
         check_serde(
             json!({
@@ -119,25 +143,83 @@ mod tests {
                 "jsonrpc": "2.0",
                 "method": "method",
                 "params": {"p0": 0, "p1": 1},
-                "id": 1
             }),
-            Request::new("method", Some(Params { p0: 0, p1: 1 }), Some(1)),
+            Notification::new("method", Some(Params { p0: 0, p1: 1 })),
         );
         check_serde(
             json!({
                 "jsonrpc": "2.0",
                 "method": "method",
+            }),
+            Notification::<()>::new("method", None),
+        );
+    }
+
+    #[test]
+    fn test_response_serde() {
+        check_serde(
+            json!({
+                "jsonrpc": "2.0",
+                "result": 1,
                 "id": 1
             }),
-            Request::<()>::new("method", None, Some(1)),
+            Response::new(JsonRPCResult::<u32, ()>::Result(1), Some(1)),
         );
         check_serde(
             json!({
                 "jsonrpc": "2.0",
-                "method": "method",
-                "id": null,
+                "result": 1,
+                "id": null
             }),
-            Request::<()>::new("method", None, None),
+            Response::new(JsonRPCResult::<u32, ()>::Result(1), None),
+        );
+
+        check_serde(
+            json!({
+                "jsonrpc": "2.0",
+                "error": {
+                    "code": -1,
+                    "message": "message"
+                },
+                "id": 2
+            }),
+            Response::new(
+                JsonRPCResult::<(), ()>::Error(JsonRPCError {
+                    code: -1,
+                    message: "message".to_string(),
+                    data: None,
+                }),
+                Some(2),
+            ),
+        );
+
+        #[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
+        struct ErrorData {
+            error_data: String,
+        }
+
+        check_serde(
+            json!({
+                "jsonrpc": "2.0",
+                "error": {
+                    "code": -1,
+                    "message": "message",
+                    "data": {
+                        "error_data": "error_data"
+                    }
+                },
+                "id": 3
+            }),
+            Response::new(
+                JsonRPCResult::<(), ErrorData>::Error(JsonRPCError {
+                    code: -1,
+                    message: "message".to_string(),
+                    data: Some(ErrorData {
+                        error_data: "error_data".to_string(),
+                    }),
+                }),
+                Some(3),
+            ),
         );
     }
 }
