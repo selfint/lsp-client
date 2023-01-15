ae2343f1a37b041b0d6ed9bc7c15fe70fadb66e7
Tom Selfin <selfint@gmail.com>
2023-01-15 12:33:47 +0200

    split jsonrpc-types from lsp-client crate

---
+++ lsp-client/src/lib.rs
@@ -0,0 +1,8 @@
+mod client;
+pub mod lsp_protocol;
+pub mod server_proxy;
+
+pub use jsonrpc_types;
+pub use lsp_types;
+
+pub use client::LspClient;
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod client;
-pub mod jsonrpc_types;
-
-pub use client::LspClient;
-pub use lsp_types;
-pub mod lsp_protocol;
-pub mod server_proxy;
